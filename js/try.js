66fa6da81c1be87a95a5069c6dcb7f565d17dc7a
kamal <karkibai600@gmail.com>
2017-07-13 13:01:55 +0545

    form

---
+++ js/try.js
@@ -0,0 +1,14 @@
+// document.getElementById('init').innerHTML=new Date();
+alert("hello");
+console.log();
+
+
+function changeContents(){
+  document.getElementById('init').innerHTML=new Date();
+
+
+
+
+
+}
+changeContents();
